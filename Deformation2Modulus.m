122bbe839c8ef2af2a10735483077b307039be2d
guerinluke <75700888+guerinluke@users.noreply.github.com>
2022-04-15 12:07:24 +0100

    Add files via upload

---
+++ Deformation2Modulus.m
@@ -0,0 +1,38 @@
+close all
+clear
+
+% Vertical deformation of gel (m)
+d = 0.001;
+% Radius of indenter (m)
+R = 0.001;
+% Density of indenter material
+p = 1;
+% Force applied by indenter (N)
+w = ((4*pi*R*R*R)/3)*p*9.81;
+% a/R ratio of gel
+a = 5;
+% gel thickness (m)
+h = 0.001;
+
+X = d/R;
+% Calculating polynomial parameters
+B0 = 0.05668*a + 0.1522 - X;
+B1 = (19653 + 8143*a + 845*a.*a);
+B2 = -(8500165149+ 3261047248*a - 310079383*a.*a);
+
+% Determining the Roots
+Y = roots([B2 B1 B0]);
+% Modulus in Pa
+E = (6*w)/(Y*h*R)
+
+% Plotting relevant a/R curve
+Y1 = [0:0.00001:0.0001];
+X1 = 0.05668*a + 0.1522+(19653 + 8143*a + 845*a.*a)*Y1 ...
+    - (8500165149+ 3261047248*a - 310079383*a.*a)*Y1.*Y1;
+
+plot(Y1,X1,'DisplayName', strcat('a/R = ', num2str(a))); hold on;
+axis([0 0.000005 0 1]);
+ylabel('δ/R'); xlabel('6w/EhR');
+title('X (δ/R) and Y (6w/EhR)');
+legend('show')
+
